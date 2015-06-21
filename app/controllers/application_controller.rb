88187ea831888606f6348efcf2b31a112b54ccf4
Łukasz Kamiński <kaminski91@gmail.com>
2015-06-21 21:36:09 +0200

    admin fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :rotator
+
+  def rotator
+    @start_gallery = Gallery.find_by(name: "start")
+  end
 end
